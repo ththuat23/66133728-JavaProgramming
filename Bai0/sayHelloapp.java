68bf82e42936d1a6f013bd1b90af1d85e28e2c4d
ththuat23 <thuatcraft@gmail.com>
2026-02-01 22:40:04 +0700

    Đổi tên bài đầu tiên thành Bài0
    Chuyển từ Bai1_hello sang Bai0, cập nhật kế hoạch làm bài tập.

---
+++ Bai0/sayHelloapp.java
@@ -0,0 +1,8 @@
+package Bai0;
+
+public class sayHelloapp{
+    public static void main(String[] args) {
+        System.out.println("Xin cao, toi la SV khoa 66 CNTT 2 - NTU. Hoc lap trinh JAva.");
+        System.out.println("Trần Huỳnh Thuật");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # 66133728-JavaProgramming
 Test 0
+Kế hoạch làm bài tập: Bài 0, Bài 1, Bài 2.
