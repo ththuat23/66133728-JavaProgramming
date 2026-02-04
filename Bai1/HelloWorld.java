22a9c565f1bc6a9cd8ed5ba090f3fa99e65b8c9c
ththuat23 <thuatcraft@gmail.com>
2026-02-04 12:57:18 +0700

    Tạo Bài 1 HelloWorld

---
+++ Bai1/HelloWorld.java
@@ -0,0 +1,8 @@
+package Bai1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+            System.out.println("Xin chào thế giới Java!");
+            System.out.println("Chào mừng đến thế giới lập trình");
+    }
+}
