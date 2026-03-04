437551623e788db848afa9e9fbca645ab758b552
ththuat23 <thuatcraft@gmail.com>
2026-03-04 15:40:26 +0700

    Create BT2.java

---
+++ Bai2/BT2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BT2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int usd, eur;
+        int vndFromUSD, vndFromEUR;
+
+        System.out.print("Nhap so tien USD: ");
+        usd = sc.nextInt();
+
+        System.out.print("Nhap so tien EUR: ");
+        eur = sc.nextInt();
+    }
+}
