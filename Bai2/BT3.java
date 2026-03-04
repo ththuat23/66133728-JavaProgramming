696e93824137d451721c16607b06024739b31f4e
ththuat23 <thuatcraft@gmail.com>
2026-03-04 21:55:32 +0700

    Update BT3.java

--- Bai2/BT3.java
+++ Bai2/BT3.java
@@ -11,5 +11,16 @@ public class BT3 {
 
         System.out.print("Nhap b: ");
         b = sc.nextDouble();
+
+        if (a == 0) {
+            if (b == 0) {
+                System.out.println("Phuong trinh vo so nghiem");
+            } else {
+                System.out.println("Phuong trinh vo nghiem");
+            }
+        } else {
+            x = -b / a;
+            System.out.println("Nghiem x = " + x);
+        }
     }
 }
