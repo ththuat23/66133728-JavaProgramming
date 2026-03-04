3c3c392c5c0e420a1713077c80ca617f69862ad8
ththuat23 <thuatcraft@gmail.com>
2026-03-04 08:31:36 +0700

    update

---
+++ Bai1/HinhVe.java
@@ -0,0 +1,10 @@
+package Bai1;
+
+public class HinhVe {
+    public static void main(String[] args) {
+        System.out.println("   *   ");
+        System.out.println("  ***  ");
+        System.out.println(" ***** ");
+        System.out.println("*******");
+    }
+}
---
+++ Bai1/TinhToan.java
@@ -0,0 +1,10 @@
+package Bai1;
+
+public class TinhToan {
+    public static void main(String[] args) {
+        System.out.println("1 + 1 = " + (1+1));
+        System.out.println("2 * 2 = " + (2*2));
+        System.out.println("3 - 2 = " + (3-2));
+        System.out.println("8 / 4 = " + (8/4));
+    }
+}
