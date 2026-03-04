4e0724a61df4f2a46efc1de1235211c7112911bf
ththuat23 <thuatcraft@gmail.com>
2026-03-04 21:41:54 +0700

    Create BT3.java

---
+++ Bai2/BT3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BT3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double a, b, x;
+
+        System.out.print("Nhap a: ");
+        a = sc.nextDouble();
+
+        System.out.print("Nhap b: ");
+        b = sc.nextDouble();
+    }
+}
