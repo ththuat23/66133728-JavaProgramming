b0e6703b544a4b74fb1860b147a46812aa2bbe0a
ththuat23 <thuatcraft@gmail.com>
2026-03-09 22:14:43 +0700

    Update TinhTienDien.java

--- Bai3_TinhTienDien/TinhTienDien.java
+++ Bai3_TinhTienDien/TinhTienDien.java
@@ -17,8 +17,21 @@ public class TinhTienDien {
         else if (kwh <= 100) {
             tien = 50 * 1806 + (kwh - 50) * 1866;
         } 
-        else {
+        else if (kwh <= 200) {
             tien = 50 * 1806 + 50 * 1866 + (kwh - 100) * 2167;
         } 
+        else if (kwh <= 300) {
+            tien = 50 * 1806 + 50 * 1866 + 100 * 2167 + (kwh - 200) * 2729;
+        } 
+        else if (kwh <= 400) {
+            tien = 50 * 1806 + 50 * 1866 + 100 * 2167 + 100 * 2729 + (kwh - 300) * 3050;
+        } 
+        else {
+            tien = 50 * 1806 + 50 * 1866 + 100 * 2167 + 100 * 2729 + 100 * 3050 + (kwh - 400) * 3151;
+        }
+
+        System.out.printf("Tien dien phai tra: %.2f VND", tien);
+
+        scanner.close();
     }
 }
