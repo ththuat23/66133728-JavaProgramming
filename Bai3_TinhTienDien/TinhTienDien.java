70ec1c59d51dabaa6516661cfa0910307bec1ae3
ththuat23 <thuatcraft@gmail.com>
2026-03-09 13:52:10 +0700

    Create TinhTienDien.java

---
+++ Bai3_TinhTienDien/TinhTienDien.java
@@ -0,0 +1,14 @@
+package Bai3_TinhTienDien;
+
+import java.util.Scanner;
+
+public class TinhTienDien {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhap so dien tieu thu (kWh): ");
+        int kwh = scanner.nextInt();
+
+        double tien = 0;
+    }
+}
