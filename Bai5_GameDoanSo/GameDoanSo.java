51e9e7f950f2628ad5fa7e15664eac7cf0b9dfbd
ththuat23 <thuatcraft@gmail.com>
2026-03-10 18:19:47 +0700

    Update GameDoanSo.java

--- Bai5_GameDoanSo/GameDoanSo.java
+++ Bai5_GameDoanSo/GameDoanSo.java
@@ -8,5 +8,38 @@ public class GameDoanSo {
 
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
+
+        String playAgain;
+
+        do {
+            int secretNumber = random.nextInt(100) + 1;
+            int guess;
+            int attempts = 0;
+            int maxAttempts = 10;
+            boolean win = false;
+
+            System.out.println("=== GAME ĐOÁN SỐ ===");
+            System.out.println("Máy đã chọn số từ 1 đến 100.");
+            System.out.println("Bạn có 10 lần đoán!");
+
+            while (attempts < maxAttempts) {
+
+                System.out.print("Nhập số đoán: ");
+                guess = scanner.nextInt();
+                attempts++;
+
+                if (guess == secretNumber) {
+                    System.out.println("Chúc mừng! Bạn đoán đúng.");
+                    win = true;
+                    break;
+                }
+                else if (guess < secretNumber) {
+                    System.out.println("Số bí mật lớn hơn!");
+                } 
+                else {
+                    System.out.println("Số bí mật nhỏ hơn!");
+                }
+            }
+        }
     }
 }
