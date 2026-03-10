c47fd103f007c0feac43654e8df56ad25359515b
ththuat23 <thuatcraft@gmail.com>
2026-03-10 13:29:53 +0700

    Update NhatKyCaNhan.java

---
+++ Bai3_NhatKyCaNhan/NhatKyCaNhan.java
@@ -0,0 +1,12 @@
+package Bai3_NhatKyCaNhan;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class NhatKyCaNhan {
+    public static void main(String[] args) {
+
+    }
+}
