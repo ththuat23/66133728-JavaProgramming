eb142dd7479594532ff29063d04ef3b42e9116eb
ththuat23 <thuatcraft@gmail.com>
2026-03-11 14:11:31 +0700

    update EX1.1

--- EX_Circle_1_1/Circle.java
+++ EX_Circle_1_1/Circle.java
@@ -1,32 +1,43 @@
 package EX_Circle_1_1;
-/**
- * The Circle class models a circle with a radius and color.
- */
-public class Circle {  // Save as "Circle.java"
-    // private instance variable, not accessible from outside this class
-    private double radius;
-    private String color;
-    
-    // Constructors (overloaded)
-    /** Constructs a Circle instance with default value for radius and color */
-    public Circle() {  // 1st (default) constructor
-        radius = 1.0;
-        color = "red";
+
+public class Circle {
+
+    private double radius = 1.0;
+    private String color = "red";
+
+    public Circle() {
     }
-    
-    /** Constructs a Circle instance with the given radius and default color */
-    public Circle(double r) {  // 2nd constructor
-        radius = r;
-        color = "red";
+
+    public Circle(double radius) {
+        this.radius = radius;
     }
-    
-    /** Returns the radius */
+
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
     public double getRadius() {
-        return radius; 
+        return radius;
     }
-    
-    /** Returns the area of this Circle instance */
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
     public double getArea() {
-        return radius*radius*Math.PI;
+        return Math.PI * radius * radius;
+    }
+
+    public String toString() {
+        return "Circle[radius=" + radius + ", color=" + color + "]";
     }
 }
--- EX_Circle_1_1/TestCircle.java
+++ EX_Circle_1_1/TestCircle.java
@@ -1,25 +1,24 @@
 package EX_Circle_1_1;
-/**
- *  A Test Driver for the Circle class
- */
-public class TestCircle {  // Save as "TestCircle.java"
-   public static void main(String[] args) {
-      // Declare an instance of Circle class called c1.
-      // Construct the instance c1 by invoking the "default" constructor
-      // which sets its radius and color to their default value.
-      Circle c1 = new Circle();
-      // Invoke public methods on instance c1, via dot operator.
-      System.out.println("The circle has radius of " 
-         + c1.getRadius() + " and area of " + c1.getArea());
-      //The circle has radius of 1.0 and area of 3.141592653589793
-   
-      // Declare an instance of class circle called c2.
-      // Construct the instance c2 by invoking the second constructor
-      // with the given radius and default color.
-      Circle c2 = new Circle(2.0);
-      // Invoke public methods on instance c2, via dot operator.
-      System.out.println("The circle has radius of " 
-         + c2.getRadius() + " and area of " + c2.getArea());
-      //The circle has radius of 2.0 and area of 12.566370614359172
-   }
+
+public class TestCircle {
+    public static void main(String[] args) {
+
+        Circle c1 = new Circle();
+        System.out.println(c1.toString());
+        System.out.println("Area: " + c1.getArea());
+
+        Circle c2 = new Circle(2.0);
+        System.out.println(c2.toString());
+        System.out.println("Area: " + c2.getArea());
+
+        Circle c3 = new Circle(3.0, "blue");
+        System.out.println(c3.toString());
+        System.out.println("Area: " + c3.getArea());
+
+        c3.setColor("green");
+        c3.setRadius(5.0);
+
+        System.out.println("Sau khi thay đổi:");
+        System.out.println(c3.toString());
+    }
 }
