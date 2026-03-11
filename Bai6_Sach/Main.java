5c877343e0a53c55c25f880e073ab65fcab48a22
ththuat23 <thuatcraft@gmail.com>
2026-03-11 13:01:01 +0700

    Update Main.java

--- Bai6_Sach/Main.java
+++ Bai6_Sach/Main.java
@@ -1,5 +1,32 @@
 package Bai6_Sach;
 
 public class Main {
-    
+    public static void main(String[] args) {
+
+        // Tạo sách 1
+        Sach sach1 = new Sach();
+        sach1.tieuDe = "Lap trinh Java";
+        sach1.tacGia = "Nguyen Van A";
+        sach1.gia = 120000;
+        sach1.soTrang = 350;
+
+        // Tạo sách 2
+        Sach sach2 = new Sach();
+        sach2.tieuDe = "Cau truc du lieu";
+        sach2.tacGia = "Tran Van B";
+        sach2.gia = 150000;
+        sach2.soTrang = 420;
+
+        // Tạo sách 3
+        Sach sach3 = new Sach();
+        sach3.tieuDe = "Lap trinh Web";
+        sach3.tacGia = "Le Van C";
+        sach3.gia = 180000;
+        sach3.soTrang = 500;
+
+        // Hiển thị thông tin
+        sach1.hienThiThongTin();
+        sach2.hienThiThongTin();
+        sach3.hienThiThongTin();
+    }
 }
