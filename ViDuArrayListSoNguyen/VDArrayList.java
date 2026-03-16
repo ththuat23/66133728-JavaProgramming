35ad22d6b39e725199d3be70460247f2160019f7
ththuat23 <thuatcraft@gmail.com>
2026-03-16 21:14:59 +0700

    Create VDArrayList.java

---
+++ ViDuArrayListSoNguyen/VDArrayList.java
@@ -0,0 +1,11 @@
+package ViDuArrayListSoNguyen;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class VDArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> dsSoNguyen = new ArrayList<Integer>();
+        Scanner banPhim = new Scanner(System.in);
+    }
+}
