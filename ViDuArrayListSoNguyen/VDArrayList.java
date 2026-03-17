9502b04bc8b73edfcbb3065a42373075df095ccb
ththuat23 <thuatcraft@gmail.com>
2026-03-17 13:23:01 +0700

    Update VDArrayList.java

--- ViDuArrayListSoNguyen/VDArrayList.java
+++ ViDuArrayListSoNguyen/VDArrayList.java
@@ -7,5 +7,16 @@ public class VDArrayList {
     public static void main(String[] args) {
         ArrayList<Integer> dsSoNguyen = new ArrayList<Integer>();
         Scanner banPhim = new Scanner(System.in);
+        
+        for (int i=0; i<10; i++) {
+            System.out.println("Mời nhập phần tử thứ "+ i + ": ");
+        }
+        Integer tam = banPhim.nextInt();
+        dsSoNguyen.add(tam);
+
+        for (int i=0; i<dsSoNguyen.size(); i++) {
+            Integer tam = dsSoNguyen.get(i);
+            System.out.println(tam + " ");
+        }
     }
 }
