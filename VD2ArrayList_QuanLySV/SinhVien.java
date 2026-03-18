23915145018cdba331b485e5e5e4ba95a02168fd
ththuat23 <thuatcraft@gmail.com>
2026-03-18 19:28:11 +0700

    Sửa SinhVien.java và copy QuanLySinhVien.java

--- VD2ArrayList_QuanLySV/SinhVien.java
+++ VD2ArrayList_QuanLySV/SinhVien.java
@@ -1,27 +1,23 @@
 package VD2ArrayList_QuanLySV;
 
-import java.util.ArrayList;
-import java.util.List;
-
-// Lớp SinhVien
 class SinhVien {
     private String maSV;
     private String hoTen;
     private double diemTB;
-    
+
     public SinhVien(String maSV, String hoTen, double diemTB) {
         this.maSV = maSV;
         this.hoTen = hoTen;
         this.diemTB = diemTB;
     }
-    
+
     // Getter
     public String getMaSV() { return maSV; }
     public String getHoTen() { return hoTen; }
     public double getDiemTB() { return diemTB; }
-    
+
     @Override
     public String toString() {
-        return String.format("Mã: ");
+        return String.format("Mã: %s | Họ tên: %s | Điểm TB: %.2f", maSV, hoTen, diemTB);
     }
 }
