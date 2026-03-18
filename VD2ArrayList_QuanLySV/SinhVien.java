069cd9f7e40a92b4db0a0969910423ede6097a78
ththuat23 <thuatcraft@gmail.com>
2026-03-18 18:32:07 +0700

    Khởi tạo

---
+++ VD2ArrayList_QuanLySV/SinhVien.java
@@ -0,0 +1,27 @@
+package VD2ArrayList_QuanLySV;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Lớp SinhVien
+class SinhVien {
+    private String maSV;
+    private String hoTen;
+    private double diemTB;
+    
+    public SinhVien(String maSV, String hoTen, double diemTB) {
+        this.maSV = maSV;
+        this.hoTen = hoTen;
+        this.diemTB = diemTB;
+    }
+    
+    // Getter
+    public String getMaSV() { return maSV; }
+    public String getHoTen() { return hoTen; }
+    public double getDiemTB() { return diemTB; }
+    
+    @Override
+    public String toString() {
+        return String.format("Mã: ");
+    }
+}
