a7f70c62b1f0c09d340586301387ca6c10ebabd4
ththuat23 <thuatcraft@gmail.com>
2026-03-19 16:44:58 +0700

    Xong lớp nhân viên văn phòng + test

--- ViduOOP_KeThua_QLNV/NhanVien.java
+++ ViduOOP_KeThua_QLNV/NhanVien.java
@@ -24,12 +24,31 @@ public class NhanVien {
         chuoiXuat = chuoiXuat + "Mã NV: " + this.maNV;
         chuoiXuat = chuoiXuat + "\nHọ tên: " + this.hoTen;
         chuoiXuat = chuoiXuat + "\nLương cơ bản: " + this.luongCoBan;
-        return "NhanVien [maNV=" + maNV + ", hoTen= " + hoTen +", luongCoBan= " + luongCoBan + "]";
+        return chuoiXuat;
+        // return "NhanVien [maNV=" + maNV + ", hoTen= " + hoTen +", luongCoBan= " + luongCoBan + "]";
     }
 
     public void hienThiThongTin() {
         System.out.println("Mã NV: " + maNV);
         System.out.println("Họ tên: " + hoTen);
         System.out.println("Lương cơ bản: " + luongCoBan);
+    }
+    public String getMaNV() {
+        return maNV;
+    }
+    public void setMaNV(String maNV) {
+        this.maNV = maNV;
+    }
+    public String getHoTen() {
+        return hoTen;
+    }
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+    public double getLuongCoBan() {
+        return luongCoBan;
+    }
+    public void setLuongCoBan(double luongCoBan) {
+        this.luongCoBan = luongCoBan;
     }
 }
--- ViduOOP_KeThua_QLNV/Run.java
+++ ViduOOP_KeThua_QLNV/Run.java
@@ -2,8 +2,12 @@ package ViduOOP_KeThua_QLNV;
 
 public class Run {
     public static void main(String[] args) {
-        NhanVien nv1 = new NhanVien("001", "Trần Huỳnh Thuật", "2300000");
+        NhanVien nv1 = new NhanVien("001", "Trần Huỳnh Thuật", 2300000.0);
         // nv1.hienThiThongTin();
         System.out.println(nv1.toString());
+
+        NhanVienVanPhong nvVanPhong1 = new NhanVienVanPhong("002", "Trần Thị Tươi Thắm", 230000, 3, 200000);
+        System.out.println(nvVanPhong1.toString());
+        // nvVanPhong1.hienThiThongTin();
     }
 }
