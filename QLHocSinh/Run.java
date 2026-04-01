b804bf7e3f749cfe2597a9c27adf041f2a4e5b0d
ththuat23 <thuatcraft@gmail.com>
2026-04-01 21:27:13 +0700

    Hoàn thành và test
    đã thêm các câu lệnh nhập từ bàn phím, đã thêm nextLine sau các nextInt, nextShort

--- QLHocSinh/Run.java
+++ QLHocSinh/Run.java
@@ -1,5 +1,6 @@
 package QLHocSinh;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Run {
@@ -25,6 +26,7 @@ public class Run {
         System.out.println("Lớp của hs 2 là: "+ hs2.getLopHS() );
 
 
+        // tạo học sinh 3, nhập thông tin từ bàn phím
         Scanner banPhim = new Scanner(System.in);
         HocSinh hs3 = new HocSinh();
 
@@ -42,7 +44,77 @@ public class Run {
         hs3.setLopHS(lopHS3);
 
         System.out.println(hs3.toString());
+
+        // Tạo danh sách N học sinh, nhập dữ liệu vào từ bàn phím
+        // từ đây mới có danh sách học sinh, từ trước là không có ai trong list dsHocSinh
+        ArrayList<HocSinh> dsHocSinh = new ArrayList<HocSinh>();
+        System.out.println("Mời nhập số lượng N học sinh: ");
+        int N = banPhim.nextInt();
+        banPhim.nextLine();
+
+        for (int i=0; i<N; i++ ){
+            HocSinh hsi = new HocSinh();
+            System.out.println("Mời nhập tên của học sinh: "+ i);
+            String tenHSi = banPhim.nextLine();
+            hsi.setTenHS(tenHSi);
+            
+            System.out.println("Mời nhập tuổi của học sinh: "+ i);
+            short tuoiHSi = banPhim.nextShort();
+            hsi.setTuoiHS(tuoiHSi);
+            banPhim.nextLine(); //Thêm dòng này để nhập được lớp
+
+            System.out.println("Mời nhập lớp của học sinh: "+ i);
+            String lopHSi = banPhim.nextLine();
+            hsi.setLopHS(lopHSi);
+            
+            dsHocSinh.add(hsi);
+        }
+
+        for (int i=0; i<N; i++){
+            System.out.println(dsHocSinh.get(i).toString());
+        }
+
+        //4a=================================
+        // nhập học sinh thêm từ bàn phím
+        HocSinh hsThem = new HocSinh();
+            System.out.println("Mời nhập tên của học sinh thêm: ");
+            String tenHSThem = banPhim.nextLine();
+            hsThem.setTenHS(tenHSThem);
+            
+            System.out.println("Mời nhập tuổi của học sinh thêm: ");
+            short tuoiHSThem = banPhim.nextShort();
+            hsThem.setTuoiHS(tuoiHSThem);
+            banPhim.nextLine(); //Thêm dòng này để nhập được lớp
+
+            System.out.println("Mời nhập lớp của học sinh thêm: ");
+            String lopHSThem = banPhim.nextLine();
+            hsThem.setLopHS(lopHSThem);
+        //Thêm vào ds
+        dsHocSinh.add(hsThem);
+        //xuất học sinh
+        System.out.println(hsThem.toString());
+
+        // in ra
+        for (int i=0; i<dsHocSinh.size(); i++){
+            System.out.println(dsHocSinh.get(i).toString());
+        }
+
+        //5a======================
+        // tìm và xóa học sinh tên Hoa trong ds
+        for (int i=0; i<N; i++ ){
+            HocSinh hsTim = dsHocSinh.get(i);
+            if (hsTim.getTenHS().equals("Hoa")) {
+                dsHocSinh.remove(hsTim);
+                break;
+            }
+        }
         
+        System.out.println("Sau cùng, danh sách học sinh là: ");
+        // in ra lại danh sách học sinh
+        for (int i=0; i<dsHocSinh.size(); i++){
+            System.out.println(dsHocSinh.get(i).toString());
+        }
+
         banPhim.close();
     }
 }
