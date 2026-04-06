2dd804caf15e88c317ec448b93c90cc6017520ee
ththuat23 <thuatcraft@gmail.com>
2026-04-06 12:04:53 +0700

    thêm 2 class interface

---
+++ Bai10_HeThongThietBiDienTu/CoTheCamUng.java
@@ -0,0 +1,6 @@
+package Bai10_HeThongThietBiDienTu;
+
+public interface CoTheCamUng {
+    void cham();
+    void vuot();
+}
---
+++ Bai10_HeThongThietBiDienTu/CoTheKetNoiInternet.java
@@ -0,0 +1,6 @@
+package Bai10_HeThongThietBiDienTu;
+
+public interface CoTheKetNoiInternet {
+    void ketNoiWifi();
+    void ngatKetNoi();
+}
