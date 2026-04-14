8fa4438cadf950c56f5d2de73586a2ca6e61c9a7
ththuat23 <thuatcraft@gmail.com>
2026-04-14 20:18:05 +0700

    cập nhật

--- ViDuStreamDocChuoi/ViDuDocStream.java
+++ ViDuStreamDocChuoi/ViDuDocStream.java
@@ -1,19 +1,27 @@
 package ViDuStreamDocChuoi;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ViDuDocStream {
     public static void main(String[] args) throws IOException {
+
+        FileInputStream fInput = new FileInputStream("baitho.txt");
+
         // Khai báo biến đối tượng
         InputStreamReader ipReader = new InputStreamReader(System.in);
         BufferedReader bufReader = new BufferedReader(ipReader);
-        // In 1 câu hướng dẫn để User biết cần phải làm gì
-        System.out.println("Nhập một chuỗi bất kỳ: ");
-        // Đọc dữ liệu từ bàn phím, cất các biến chuỗi
-        String lineDocDuoc = bufReader.readLine();
-        
+
+        // Đọc dữ liệu từ file, cất các biến chuỗi
+        String line1 = bufReader.readLine();
+        // In ra dòng đọc được
+        System.out.println(line1);
+        String line2 = bufReader.readLine();
+        // In ra dòng đọc được
+        System.out.println(line2);
 
     }
 }
