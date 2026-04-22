0652f40d9da3a980ce98081b4bfca06b8894a378
ththuat23 <thuatcraft@gmail.com>
2026-04-22 23:17:19 +0700

    LẦN THỨ 2
    Tạo getter/setter + tính tiền + xuất giá trị

--- OnTapGiuaKi2/TranHuynhThuat/SanPham.java
+++ OnTapGiuaKi2/TranHuynhThuat/SanPham.java
@@ -15,4 +15,50 @@ public class SanPham {
         this.gia = gia;
         this.soLuong = soLuong;
     }
+    //Getter và Setter
+    public String getMaSP() {
+        return this.maSP;
+    }
+
+    public void setMaSP(String maSP) {
+        this.maSP = maSP;
+    }
+
+    public String getTenSP() {
+        return this.tenSP;
+    }
+
+    public void setTenSP(String tenSP) {
+        this.tenSP = tenSP;
+    }
+
+    public double getGia() {
+        return this.gia;
+    }
+
+    public void setGia(double gia) {
+        this.gia = gia;
+    }
+
+    public int getSoLuong() {
+        return this.soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    // Tính thành tiền
+    public double tinhThanhTien() {
+        return gia * soLuong;
+    }
+
+    // Hiển thị
+    public void hienThiThongTin() {
+        System.out.println("Mã SP: " + maSP);
+        System.out.println("Tên SP: " + tenSP);
+        System.out.println("Giá: " + gia);
+        System.out.println("Số lượng: " + soLuong);
+        System.out.println("Thành tiền: " + tinhThanhTien());
+    }
 }
