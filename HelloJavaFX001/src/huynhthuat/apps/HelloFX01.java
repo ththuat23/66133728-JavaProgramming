0bd13a3ae7543bafee8057c38e32d815d3d572df
ththuat23 <thuatcraft@gmail.com>
2026-05-20 20:09:44 +0700

    Xong ví dụ 1 JavaFX

---
+++ HelloJavaFX001/src/huynhthuat/apps/HelloFX01.java
@@ -0,0 +1,29 @@
+package huynhthuat.apps;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class HelloFX01 extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        Button btn = new Button("Xin Chao FX");
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 400, 200);
+
+        primaryStage.setTitle("Hello JavaFX");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
